8aa109ace8111dffd61267c0b822ac0d0130fa69
Aurore Dimech <aurore.dimech@edu.devinci.fr>
2025-07-07 12:13:23 +0200

    first tets

---
+++ client/client/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <h1>Rock-Paper-Scissors Realtime</h1>
+    <Game />
+  </div>
+</template>
+
+<script>
+import Game from './components/Game.vue';
+
+export default {
+  components: { Game }
+};
+</script>
---
+++ client/client/src/components/Game.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <h2>Choose your move:</h2>
+    <button @click="play('rock')">🪨 Rock</button>
+    <button @click="play('paper')">📄 Paper</button>
+    <button @click="play('scissors')">✂️ Scissors</button>
+
+    <p v-if="waitingForOpponent">Waiting for opponent...</p>
+
+    <p v-if="result">
+      You played: {{ playerMove }} <br />
+      Opponent played: {{ opponentMove }} <br />
+      Result: <strong>{{ resultText }}</strong>
+    </p>
+  </div>
+</template>
+
+<script>
+import { socket } from '../socket';
+
+export default {
+  data() {
+    return {
+      room: null,
+      playerMove: null,
+      opponentMove: null,
+      waitingForOpponent: false,
+      result: null,
+    };
+  },
+  mounted() {
+    socket.on('waiting', () => {
+      this.waitingForOpponent = true;
+    });
+    socket.on('start-game', ({ room }) => {
+      this.room = room;
+      this.waitingForOpponent = false;
+      this.result = null;
+      this.playerMove = null;
+      this.opponentMove = null;
+    });
+    socket.on('opponent-played', (move) => {
+      this.opponentMove = move;
+    });
+    socket.on('result', ({ p1Move, p2Move, winner }) => {
+      this.playerMove = p1Move; // assuming current player is player1
+      this.opponentMove = p2Move;
+      if (winner === 'draw') {
+        this.result = 'draw';
+      } else if (winner === 'player1') {
+        this.result = 'win';
+      } else {
+        this.result = 'lose';
+      }
+      this.waitingForOpponent = false;
+    });
+  },
+  methods: {
+    play(move) {
+      if (!this.room) return alert('Waiting for an opponent...');
+      this.playerMove = move;
+      this.waitingForOpponent = true;
+      socket.emit('play', { move, room: this.room });
+    }
+  }
+};
+</script>
