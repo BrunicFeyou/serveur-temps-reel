7108082a2f1f3014707f10e479dc13ceaf9d761b
Aurore Dimech <aurore.dimech@edu.devinci.fr>
2025-07-08 09:18:27 +0200

    feat: camera with ai

--- client/src/components/Game.vue
+++ client/src/components/Game.vue
@@ -20,6 +20,16 @@
       <button @click="play('rock')">🪨 Rock</button>
       <button @click="play('paper')">📄 Paper</button>
       <button @click="play('scissors')">✂️ Scissors</button>
+      <button @click="startCamera">📷 Play with Camera</button>
+      <div v-if="showCamera" class="camera-section">
+        <video ref="video" autoplay playsinline width="224" height="224"></video>
+        <button @click="predictMove">Predict Move</button>
+        <button @click="stopCamera">Close Camera</button>
+        <p v-if="predictedMove">Predicted: {{ predictedMove }}</p>
+        <button v-if="predictedMove" @click="validatePredictedMove">Validate</button>
+      </div>
+      <canvas ref="canvas" width="224" height="224" style="display:none;"></canvas>
+      <p v-if="playerMove">You played: {{ playerMove }}</p>
       <p v-if="opponentMove">Opponent played: {{ opponentMove }}</p>
       <p v-if="roundResult" class="round-result">{{ roundResult }}</p>
     </div>
@@ -28,6 +38,7 @@
 
 <script>
 import { socket } from '../socket';
+let model = null;
 
 export default {
   data() {
@@ -39,13 +50,15 @@ export default {
       error: '',
       scores: { you: 0, opponent: 0 },
       roundResult: '',
+      showCamera: false,
+      predictedMove: '',
+      playerMove: '',
     };
   },
   mounted() {
     socket.on('game-created', (code) => {
       this.roomCode = code;
     });
-
     socket.on('game-start', ({ roomCode }) => {
       this.roomCode = roomCode;
       this.gameStarted = true;
@@ -53,8 +66,8 @@ export default {
       this.scores = { you: 0, opponent: 0 };
       this.roundResult = '';
       this.opponentMove = '';
+      this.playerMove = '';
     });
-
     socket.on('opponent-played', (payload) => {
       if (typeof payload === 'object' && payload !== null) {
         this.opponentMove = payload.move;
@@ -67,11 +80,9 @@ export default {
           this.roundResult = 'Opponent wins this round!';
         }
       } else {
-        // fallback for old server
         this.opponentMove = payload;
       }
     });
-
     socket.on('error-message', (msg) => {
       this.error = msg;
     });
@@ -90,7 +101,53 @@ export default {
     },
     play(move) {
       if (!this.roomCode) return;
+      this.playerMove = move;
       socket.emit('play', { roomCode: this.roomCode, move });
+    },
+    async startCamera() {
+      this.showCamera = true;
+      this.predictedMove = '';
+      if (!model) {
+        // Lazy-load tfjs and the model
+        const tf = await import('@tensorflow/tfjs');
+        model = await tf.loadLayersModel('/model/model.json');
+      }
+      await this.$nextTick();
+      const video = this.$refs.video;
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        this.stream = await navigator.mediaDevices.getUserMedia({ video: { width: 224, height: 224 } });
+        video.srcObject = this.stream;
+      }
+    },
+    stopCamera() {
+      this.showCamera = false;
+      this.predictedMove = '';
+      if (this.stream) {
+        this.stream.getTracks().forEach(track => track.stop());
+        this.stream = null;
+      }
+    },
+    async predictMove() {
+      const tf = await import('@tensorflow/tfjs');
+      const video = this.$refs.video;
+      const canvas = this.$refs.canvas;
+      const ctx = canvas.getContext('2d', { willReadFrequently: true });
+      ctx.drawImage(video, 0, 0, 224, 224);
+      const imageData = ctx.getImageData(0, 0, 224, 224);
+      let input = tf.browser.fromPixels(imageData).expandDims(0).toFloat().div(255);
+      const prediction = await model.predict(input).data();
+      const classes = ['rock', 'paper', 'scissors'];
+      const maxIdx = prediction.indexOf(Math.max(...prediction));
+      const move = classes[maxIdx];
+      this.predictedMove = move;
+      // Do not send the move yet, wait for user to validate
+    },
+    validatePredictedMove() {
+      if (this.predictedMove) {
+        this.play(this.predictedMove); // play() will set playerMove
+        this.stopCamera();
+        this.predictedMove = '';
+      }
     }
   }
 };
@@ -134,5 +191,9 @@ input {
 .round-result {
   font-weight: bold;
   margin-top: 10px;
+}
+
+.camera-section {
+  margin: 10px 0;
 }
 </style>
